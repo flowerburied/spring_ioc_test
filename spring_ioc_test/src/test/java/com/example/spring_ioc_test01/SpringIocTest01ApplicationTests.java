9cc7b45f7b7b0396a200d7a4f0716d7061c5d88e
flowerburied <2842511561@qq.com>
2022-11-18 14:59:55 +0800

    first commit

---
+++ spring_ioc_test/src/test/java/com/example/spring_ioc_test01/SpringIocTest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_ioc_test01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIocTest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
